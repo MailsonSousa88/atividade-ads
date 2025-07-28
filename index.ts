202b4a3ff04035639eefca8ec38ef1f91f42576a
MailsonSousa88 <legionslmax86@gmail.com>
2025-07-28 20:09:46 -0300

    Questão sobre classe de conta e transferência

---
+++ index.ts
@@ -0,0 +1,29 @@
+//Crie uma classe Conta com método de transferir(valor:number/destino:Conta) que transfere um valor de uma conta para outra
+class Conta {
+    public titular : string;
+    public saldo : number;
+
+    constructor(titularDaConta:string, saldoDoTitular:number){
+        this.titular = titularDaConta;
+        this.saldo = saldoDoTitular;
+    }
+
+    tranferir(valor:number, destino:Conta): void{
+        if(valor > this.saldo){
+            //não pode ser executado, saldo negativo
+            console.log(`A transferencia foi negada, o valor de transferencia nao pode ser processado!`);
+            return;
+        }
+        this.saldo -= valor;
+        destino.saldo += valor;
+        console.log(`A transferencia do valor de R$${valor} feita por ${this.titular} foi um sucesso!`);
+    }
+}
+
+//criando as contas
+let pessoa1 = new Conta("Jonas", 10000);
+let pessoa2 = new Conta("Julia", 3000);
+
+pessoa1.tranferir(1000, pessoa2);//executando a transferencia para outra conta
+
+console.log(`${pessoa2.titular} esta com saldo atual de R$${pessoa2.saldo}`);
